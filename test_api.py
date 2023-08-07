4faa437932c96fb46d500572e8c3b2adca73c5b8
Seba Besio <besio10@gmail.com>
2023-08-06 21:45:52 -0300

    Reduccion años

--- test_api.py
+++ test_api.py
@@ -2,8 +2,6 @@ import requests
 
 print(requests.get("http://127.0.0.1:8000/").json())
 
-# print(requests.get("http://127.0.0.1:8000/items/1234").json())
-
 # print(requests.get("http://127.0.0.1:8000/genero/2015").json())
 
 # print(requests.get("http://127.0.0.1:8000/juegos/2017").json())
@@ -16,13 +14,6 @@ print(requests.get("http://127.0.0.1:8000/").json())
 
 # print(requests.get("http://127.0.0.1:8000/metascore/2000").json())
 
-# url = "http://127.0.0.1:8000/prediccion/?earlyaccess=true&sentiment=%27Mostly%20Positive%27&year=2018&genre=Action&genre=Casual&genre=Indie&genre=Simulation&genre=Strategy&tags=Strategy&tags=Action&tags=Indie&tags=Casual&tags=Strategy&specs=Single-Player"
-url = "http://127.0.0.1:8000/prediccion/?earlyaccess=true&sentiment=%27Mostly%20Positive%27&year=2018&tag=Free%20to%20Play&genre=Casual&genre=Indie&genre=Simulation&genre=Strategy&tags=Strategy&tags=Action&tags=Indie&tags=Casual&tags=Strategy&specs=Single-Player"
-print(requests.get(url).json())
-
-"""
-    genre=['Action', 'Casual', 'Indie', 'Simulation', 'Strategy'],
-    tags=['Strategy', 'Action', 'Indie', 'Casual', 'Simulation'],
-    specs=['Single-player']
-)
-"""
+# prediccion_url = "http://127.0.0.1:8000/prediccion/?earlyaccess=true&sentiment=%27Mostly%20Positive%27&year=2018&genre=Action&genre=Casual&genre=Indie&genre=Simulation&genre=Strategy&tags=Strategy&tags=Action&tags=Indie&tags=Casual&tags=Strategy&specs=Single-Player"
+prediccion_url = "http://127.0.0.1:8000/prediccion/?earlyaccess=true&sentiment=%27Mostly%20Positive%27&year=2018&genre=Casual&genre=Indie&genre=Simulation&genre=Strategy&tags=Strategy&tags=Action&tags=Indie&tags=Casual&tags=Strategy&tags=Free%20to%20Play&specs=Single-Player"
+print(requests.get(prediccion_url).json())
